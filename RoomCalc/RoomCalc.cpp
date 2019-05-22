a319a7f116f4ccd124167f2ec331d5732f6c2eab
Conner <chourigan1@gmail.com>
2019-05-22 19:43:50 +0100

    fixed paint calculations

--- RoomCalc/RoomCalc.cpp
+++ RoomCalc/RoomCalc.cpp
@@ -13,7 +13,7 @@ float calcWallPaint(std::vector<float> wallsize) {
 	float wallheight;
 	float totalwallarea = 0;
 
-	std::cout << "Enter Height of walls.";
+	std::cout << "Enter Height of walls in meters. " << std::endl;
 	std::cin >> wallheight;
 
 	for (size_t i = 0; i < wallsize.size(); i++)
@@ -21,7 +21,7 @@ float calcWallPaint(std::vector<float> wallsize) {
 		totalwallarea += wallsize[i] * wallheight;
 	}
 
-	return totalwallarea;
+	return totalwallarea * 2;
 }
 
 float calcRoomVolume(std::vector<float> wallsize, int wallheight) {
@@ -37,99 +37,44 @@ int main()
 	float area;
 	const float PI = 3.142f;
 	std::vector<float> wallssize;
-	bool getinput;
+	bool getinput = true;
 
-
-
-	
-	while (getinput = true)
+	float walllength = 0;
+	getinput = true;
+	while (getinput == true)
 	{
-		std::cout << "Enter number of walls.";
-		std::cin >> walls;
+		std::cout << "Enter the Length of the room" << std::endl;
+		std::cin >> walllength;
 
 		if (std::cin)
 		{
 			getinput = false;
 		}
 		else {
-			std::cout << "Please enter a number.";
+			std::cout << "Please enter a number." << std::endl;
 		}
 	}
-	
-
-	if (walls == 1)
+	wallssize.push_back(walllength);
+	walllength = 0;
+	getinput = true;
+	while (getinput == true)
 	{
-		float walllength = 0;
+		std::cout << "Enter the Width of the room" << std::endl;
+		std::cin >> walllength;
 
-		while (getinput = true)
+		if (std::cin)
 		{
-			std::cout << "Enter the Lenght of the wall";
-			std::cin >> walllength;
-
-			if (std::cin)
-			{
-				getinput = false;
-			}
-			else {
-				std::cout << "Please enter a number.";
-			}
+			getinput = false;
 		}
-		wallssize.push_back(walllength);
-		area = PI * wallssize[0];
-		std::cout << "The area of the room is: " << area << std::endl;
-		float requiredPaint = 0;
-		requiredPaint = calcWallPaint(wallssize);
-		std::cout << "The required amount of paint for this room is: " << requiredPaint << std::endl;
-	}
-	else if (walls == 3) {
-		for (size_t i = 0; i < 2; i++)
-		{
-			float walllength = 0;
-			while (getinput = true)
-			{
-				std::cout << "Enter the Lenght of the wall";
-				std::cin >> walllength;
-
-				if (std::cin)
-				{
-					getinput = false;
-				}
-				else {
-					std::cout << "Please enter a number.";
-				}
-			}
-			wallssize.push_back(walllength);
+		else {
+			std::cout << "Please enter a number." << std::endl;
 		}
-		area = 0.5f * wallssize[0] * wallssize[1];
-		std::cout << "The area of the room is: " << area << std::endl;
-		float requiredPaint = 0;
-		requiredPaint = calcWallPaint(wallssize);
-		std::cout << "The required amount of paint for this room is: " << requiredPaint << std::endl;
 	}
-	else if (walls == 4) {
-		for (size_t i = 0; i < 2; i++)
-		{
-			float walllength = 0;
-			while (getinput = true)
-			{
-				std::cout << "Enter the Lenght of the wall";
-				std::cin >> walllength;
-
-				if (std::cin)
-				{
-					getinput = false;
-				}
-				else {
-					std::cout << "Please enter a number.";
-				}
-			}
-			wallssize.push_back(walllength);
-		}
-		area = wallssize[0] * wallssize[1];
-		std::cout << "The area of the room is: " << area << std::endl;
-		float requiredPaint = 0;
-		requiredPaint = calcWallPaint(wallssize);
-		std::cout << "The required amount of paint for this room is: " << requiredPaint << std::endl;
-	}
+	wallssize.push_back(walllength);
+	area = wallssize[0] * wallssize[1];
+	std::cout << "The area of the room is: " << area << " metres squared" << std::endl;
+	float requiredPaint = 0;
+	requiredPaint = calcWallPaint(wallssize);
+	std::cout << "The required amount of paint for this room is: " << requiredPaint << " square metres or " << requiredPaint / 14 << " litres" << std::endl;
 }
 
